fb56b9044c772460357146c21394a34685d5fd92
Shaik Aliya <skaliya205@gmail.com>
2025-03-10 16:07:51 +0530

    adds .env

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 // import API_KEY from "./API_KEY";
-const API_KEY = import.meta.env.API_KEY;
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 export const supabaseUrl = "https://ongwclrnfeefcrlxbgzy.supabase.co";
 const supabaseKey = API_KEY;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
+import { config } from "dotenv";
 
+config();
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
